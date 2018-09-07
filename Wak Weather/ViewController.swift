9f8488d027ebb3022e8e4282f2a47222ebe2ac2d
Scott Baumbich <scottbaumbich@macbook-pro.local>
2018-09-07 18:36:59 -0500

    Initial Commit

---
+++ Wak Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wak Weather
+//
+//  Created by Scott Baumbich on 9/7/18.
+//  Copyright © 2018 Keasbey Nights. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
